96e7689a4f1bfd7d2603631fba6d0c2eb44f484a
MauricioFGF <mfgf@cin.ufpe.br>
2023-05-18 09:59:44 -0300

    Adicionada rota para página não encontrada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PokemonProfileComponent } from './components/pokemon-profile/pokemon-profile.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'pokemon', redirectTo: '', pathMatch: 'full' },
   { path: 'pokemon/:id', component: PokemonProfileComponent },
+  { path: '**', component: NotfoundComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HomeComponent } from './components/home/home.component';
 import { PokedexComponent } from './components/pokedex/pokedex.component';
 import { PokemonProfileComponent } from './components/pokemon-profile/pokemon-profile.component';
 import { HeaderComponent } from './components/header/header.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { HeaderComponent } from './components/header/header.component';
     PokedexComponent,
     PokemonProfileComponent,
     HeaderComponent,
+    NotfoundComponent,
   ],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
